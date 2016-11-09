0fb2f4ff5d9e5f4e5244a0ec1f7597261fb421db
voksepp <fredrik.voksepp@gmail.com>
2016-11-09 01:26:29 +0100

    v1

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,2 +1,14 @@
-public class PriorityQueue {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
+
+public class PriorityQueue<E> {
+
+    private ArrayList<Bid> binaryHeap;
+    private Map<String, Integer> placeMap;
+    private Comparator cmp;
+
+    public PriorityQueue (Comparator<? super E> cmp){
+        this.cmp=cmp;
+    }
 }
