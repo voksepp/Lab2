2f574a4b8f4c2c0de268ebbff730aa4fef415732
Erik <erikforsstrom.kode@gmail.com>
2016-11-17 10:41:18 +0100

    2

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -11,4 +11,23 @@ public class PriorityQueue<E> {
     public PriorityQueue (Comparator<? super E> cmp){
         this.cmp=cmp;
     }
+
+    private void shiftUp(){
+        int k = binaryHeap.size() - 1;
+        while (k>0){
+            int p = (k-1)/2;
+            Bid item = binaryHeap.get(k);
+            Bid parent = binaryHeap.get(p);
+            if(item.compareTo(parent) > 0) {
+                //swap
+                binaryHeap.set(k,parent);
+                binaryHeap.set(p,item);
+                //move up one level
+                k=p;
+            } else{
+                break;
+            }
+
+        }
+    }
 }
