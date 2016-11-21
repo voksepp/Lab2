3dd9c1c5e13e110651ac7d6b28b10da23271535b
Erik <erikforsstrom.kode@gmail.com>
2016-11-21 17:58:23 +0100

    8

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -46,12 +46,27 @@ public class PriorityQueue<E> {
      * @return
      */
     public E extract(int index) { //FIXME: fixa
-        while (index < binaryHeap.size()-1) {
-            swap(index, rightChild(index));
-            index = parent(index);
+        while (index < binaryHeap.size() - 1) {
+            if(rightChild(index) != -1){
+                if (cmp.compare(binaryHeap.get(leftChild(index)), binaryHeap.get(rightChild(index))) > 0){
+                        swap(index, leftChild(index));
+                        index = leftChild(index);
+                }
+                else if (cmp.compare(binaryHeap.get(leftChild(index)), binaryHeap.get(rightChild(index))) < 0){
+                        swap(index, rightChild(index));
+                        index = rightChild(index);
+                }
+            }
+            else if (leftChild(index) != -1){
+                    swap(index, leftChild(index));
+                    index = leftChild(index);
+            }
+            else{
+                break;
+            }
         }
         return binaryHeap.get(binaryHeap.size()-1);
-    }
+}
 
     /**
      *
