ee978c945f134f3e763ad594a8080fe6f1df3357
Erik <erikforsstrom.kode@gmail.com>
2016-11-21 17:20:12 +0100

    7.3.6

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -91,14 +91,21 @@ public class PriorityQueue<E> {
     public void bubbleDown(int index) {
         while (index < binaryHeap.size()) {
             if(rightChild(index) != -1){
-               if (cmp.compare(binaryHeap.get(leftChild(index)), binaryHeap.get(rightChild(index))) > 0){
-                   swap(index, leftChild(index));
-                   index = leftChild(index);
+               if (cmp.compare(binaryHeap.get(leftChild(index)), binaryHeap.get(rightChild(index))) < 0){
+                   if(cmp.compare(binaryHeap.get(index),binaryHeap.get(leftChild(index))) < 0){
+                       swap(index, leftChild(index));
+                       index = leftChild(index);
+                   }
                }
-               else if (cmp.compare(binaryHeap.get(leftChild(index)), binaryHeap.get(rightChild(index))) < 0){
-                   swap(index, rightChild(index));
-                   index = rightChild(index);
+               else if (cmp.compare(binaryHeap.get(leftChild(index)), binaryHeap.get(rightChild(index))) > 0){
+                   if(cmp.compare(binaryHeap.get(index), binaryHeap.get(rightChild(index))) < 0){
+                       swap(index, rightChild(index));
+                       index = rightChild(index);
+                   }
                }
+            }
+            else if (leftChild(index) != -1){
+                if (cmp.compare())
             }
 
         }
