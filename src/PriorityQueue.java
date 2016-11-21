5e4c9a5c69ccc7588d5fafadb7ff7dcbe83b9afe
Erik <erikforsstrom.kode@gmail.com>
2016-11-21 15:49:28 +0100

    6.5

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,9 +1,6 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  *
@@ -39,7 +36,7 @@ public class PriorityQueue<E> {
      */
     public void replace(E e){
         if(){
-
+            
         }
         else{
             System.out.println("Felaktigt bud. Detta bud existerar ej!");
