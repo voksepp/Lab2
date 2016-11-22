21f1dfc698f9df65e7bc0ca8779b440c330e2657
Erik <erikforsstrom.kode@gmail.com>
2016-11-22 13:29:22 +0100

    11.7

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -41,7 +41,7 @@ public class PriorityQueue<E> {
 
     /**
      *
-     * @param index
+     * @param index is the index of the bid that should be bubbled up
      */
     private void bubbleUp(int index){
         while(index > 0 && cmp.compare(binaryHeap.get(index), binaryHeap.get(parent(index))) > 0) {
