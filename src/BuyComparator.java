0e6ea7e9adc215b4eba40a5cd1835aacac5535c3
voksepp <fredrik.voksepp@gmail.com>
2016-11-25 17:25:23 +0100

    v15

--- src/BuyComparator.java
+++ src/BuyComparator.java
@@ -6,12 +6,6 @@ import java.util.Comparator;
 class BuyComparator implements Comparator<Bid>{
 
     public int compare(Bid a, Bid b) {
-        if(a.getValue() < b.getValue()) {
-            return -1;
-        } else if(a.getValue() > b.getValue()) {
-            return 1;
-        } else {
-            return 0;
-        }
+        return a.getValue()-b.getValue();
     }
 }
--- src/SellComparator.java
+++ src/SellComparator.java
@@ -6,12 +6,6 @@ import java.util.Comparator;
 class SellComparator implements Comparator<Bid>{
 
     public int compare(Bid a, Bid b) {
-        if(a.getValue() > b.getValue()) {
-            return -1;
-        } else if(a.getValue() < b.getValue()) {
-            return 1;
-        } else {
-            return 0;
-        }
+        return b.getValue()-a.getValue();
     }
 }
