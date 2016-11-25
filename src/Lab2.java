54181c8297d5be5123ba63a1b40917a1b703f8e3
voksepp <fredrik.voksepp@gmail.com>
2016-11-25 15:20:12 +0100

    v15

--- src/Lab2.java
+++ src/Lab2.java
@@ -70,9 +70,13 @@ public class Lab2 {
      * @param sellQueue Priority queue of seller bids
      */
     private static void printOrderBook(PriorityQueue<Bid> buyQueue, PriorityQueue<Bid> sellQueue) {
+        PriorityQueue<Bid> buyQueue2 = buyQueue;
+        PriorityQueue<Bid> sellQueue2 = sellQueue;
+
+
         System.out.println("Orderbok:");
-        System.out.println("Säljare: " + sellQueue.toString());
-        System.out.println("Köpare: " + buyQueue.toString());
+        System.out.println("Säljare: " + sellQueue2.toString());
+        System.out.println("Köpare: " + buyQueue2.toString());
     }
 
     /**
